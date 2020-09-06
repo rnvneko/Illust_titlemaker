8581fb75d50ae1f343d9b9e0718b7b36af45e46e
rnvneko <rnvneko@gmail.com>
2020-09-06 22:50:19 +0900

    リマインダー
    定例会のお題リマインド機能の追加。
    軽微な修正。

--- bot.py
+++ bot.py
@@ -3,7 +3,11 @@
 # インストールした discord.py を読み込む
 import discord
 import illust_titlemaker1
+import practice_reminder
 import os
+from discord.ext import tasks
+from datetime import datetime, timedelta, timezone
+
 
 # 自分のBotのアクセストークンに置き換えてください
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
@@ -27,5 +31,25 @@ async def on_message(message):
     if message.content == "/お題":
         await message.channel.send(illust_titlemaker1.randomtitle())
     if message.content == "/唯ちゃん":
-        await message.channel.send("佐倉さんはちょっと苦手。。。")
+        await message.channel.send("佐倉さんはけっこう苦手。。。")
+    if message.content == "/あんこーる":
+        await message.channel.send(practice_reminder.random_member())
+
+
+JST = timezone(timedelta(hours=+9), 'JST')
+
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now(JST)
+    time = now.strftime('%H:%M')
+    day = now.weekday()
+    if time == '19:oo' and (day == 2 or day == 4):
+        channel = client.get_channel(716206634979950613)
+        await channel.send(practice_reminder.random_member())
+
+#ループ処理実行
+loop.start()
+
 client.run(TOKEN)
---
+++ practice_reminder.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import random
+
+members = ["佐倉（不良）", "p-hone", "炭素", "すばる", "rnvneko"]
+
+
+def random_member():
+    member = random.choice(members)
+    return member + "さんお題を決めてほしいな♡\nおやすみのときは「/あんこーる」って言ってね(ovo)"
+
